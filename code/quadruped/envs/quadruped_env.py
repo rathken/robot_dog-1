03af5957cb5f51c5c833f8c6fa6f59306a7ca15d
CK Lee <meetup.clee@gmail.com>
2018-11-05 06:51:15 +0000

    fixed typos. Not working yet.

--- code/quadruped/envs/quadruped_env.py
+++ code/quadruped/envs/quadruped_env.py
@@ -1,6 +1,7 @@
 import os
 import math
 import numpy as np
+
 import gym
 from gym import spaces
 from gym.utils import seeding
@@ -9,16 +10,17 @@ import pybullet as p
 import pybullet_data
 
 class QuadrupedEnv(gym.Env):
-    metadata={
+    metadata = {
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second' : 50
     }
 
     def __init__(self, render=True):
         self._observation = []
-        self._action_space = spaces.Discrete(9)
-        self._observation_space = spaces.Box(np.array([-math.pi,-math.pi, -5]),
-                                             np.array([math.pi,math.pi,5])) # pitch, gyro, com.sp.
+        self.action_space = spaces.Discrete(9)
+        self.observation_space = spaces.Box(np.array([-math.pi, -math.pi, -5]), 
+                                            np.array([math.pi, math.pi, 5])) # pitch, gyro, com.sp.
+
         if (render):
             self.physicsClient = p.connect(p.GUI)
         else:
@@ -32,29 +34,44 @@ class QuadrupedEnv(gym.Env):
         return [seed]
     
     def _step(self, action):
-        return
+        #TODO
+        p.stepSimulation()
+        self._observation = self._compute_observation()
+        reward = self._compute_reward()
+        done = self._compute_done()
+        
+        self._envStepCounter += 1
+
+        return np.array(self._observation), reward, done, {}
         
     def _reset(self):
         # reset is called once at initialization of simulation
+        self.vt = 0
+        self.vd = 0
+        self.maxV = 24.6 # 235RPM = 24,609142453 rad/sec
+        self._envStepCounter = 0
+        cameraDistance=2.0
+        cameraPitch=-35.4
+        cameraYaw=26.8
+        cameraTargetPosition=[0.4,0.2,-0.2]
+        p.resetDebugVisualizerCamera(cameraDistance,cameraYaw,cameraPitch,cameraTargetPosition)
         p.resetSimulation()
-        p.setGravity(0,0,-10) # m/s^2
+        p.setGravity(0,0,-9.8) # m/s^2
         p.setTimeStep(0.01) # sec
         planeId = p.loadURDF("plane.urdf")
-
-        cubeStartPos = [0,0,0.001]
+        cubeStartPos = [0,0,0.28]
         cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-
         path = os.path.abspath(os.path.dirname(__file__))
-        self.botId = p.loadURDF(os.path.join(path, "balancebot_simple.xml"),
-                           cubeStartPos,
-                           cubeStartOrientation)
+        self.botId = p.loadURDF("/home/bb8/robot/git/robot_dog/urdf/quadruped.urdf",                          
+                                cubeStartPos,
+                                cubeStartOrientation)
 
         # you *have* to compute and return the observation from reset()
         self._observation = self._compute_observation()
         return np.array(self._observation)
 
     def _assign_throttle(self, action):
-        return
+        pass
 
     def _compute_observation(self):
         cubePos, cubeOrn = p.getBasePositionAndOrientation(self.botId)
--- code/quadruped_rl.py
+++ code/quadruped_rl.py
@@ -2,6 +2,10 @@ import gym
 from baselines import deepq
 import quadruped
 
+def callback(lcl, glb):
+    # stop training if reward exceeds 199
+    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+    return is_solved
 
 def main():
     # create the environment
@@ -9,6 +13,20 @@ def main():
     # create the learning agent
     model = deepq.models.mlp([16,16])
 
+    # train the agent on the environment
+    act = deepq.learn( env, q_func=model,
+                       lr=1e-3,
+                       max_timesteps=200000,
+                       buffer_size=50000,
+                       exploration_fraction=0.1,
+                       exploration_final_eps=0.02,
+                       print_freq=10,
+                       callback=callback
+                       )
+    # save trained model
+    act.save("qaudruped.pkl")
+
+    
 if __name__ == '__main__':
     main()
 
